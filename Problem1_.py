09f5dd6b88e98c44eba05c9b6bf8921de0b1fd34
ddeniz0 <84727179+ddeniz0@users.noreply.github.com>
2021-05-25 01:32:01 +0300

    Python multiplication
    Python program that performs multiplication by 3 numbers.

---
+++ Problem1_.py
@@ -0,0 +1,10 @@
+a = int(input("enter the first number:  "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: "))
+
+d=c*b*a
+
+print("result : {}".format(d))
+
+input("press enter to exit")
+
