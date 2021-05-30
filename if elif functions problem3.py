19f8a2865e32bae9a5d3fc4012bb7f2bc6dfc6ab
diyar deniz kavrulmuş <84727179+ddeniz0@users.noreply.github.com>
2021-05-30 12:44:47 +0300

    if elif functions problem3
    python app that finds letter grade

---
+++ if elif functions problem3.py
@@ -0,0 +1,20 @@
+point = float(input("enter exam marks "))
+
+if point >=100:
+    print("wrong entry")
+elif point >=90:
+    print("letter grade: AA")
+elif point >=80:
+     print("letter grade: BA")
+elif point >=70:
+     print("letter grade: BB")
+elif point >=65:
+     print("letter grade: CB")
+elif point >=60:
+     print("letter grade: CC")
+elif point >=55:
+     print("letter grade: CD")
+elif point >=50:
+     print("letter grade: DD")
+else:
+    print("letter grade: FF")
